0e0119812b984bc45498b360e4f82e36fa8c2d67
Ing. Oscar Bello <oscar.bello@yahoo.com.mx>
2021-10-12 23:47:12 -0500

    Analisi Salarial

---
+++ salarios.js
@@ -0,0 +1,96 @@
+const colombia = [];
+
+colombia.push({
+    name:"Luisa",
+    salary: 140000,
+});
+
+colombia.push({
+    name:"Lisa",
+    salary: 230,
+});
+
+colombia.push({
+    name:"Leia",
+    salary: 240,
+});
+
+colombia.push({
+    name:"Leila",
+    salary: 280,
+});
+
+colombia.push({
+    name:"Lilia",
+    salary: 100,
+});
+
+colombia.push({
+    name:"Liceth",
+    salary: 500,
+});
+
+colombia.push({
+    name:"Leila",
+    salary: 200,
+});
+
+colombia.push({
+    name:"Lina",
+    salary: 500,
+});
+
+colombia.push({
+    name:"Lucas",
+    salary: 450,
+});
+
+colombia.push({
+    name:"Lulu",
+    salary:400,
+});
+
+colombia.push({
+    name:"Lorena",
+    salary: 220,
+});
+
+colombia.push({
+    name:"Lola",
+    salary: 210,
+});
+
+colombia.push({
+    name:"Lula",
+    salary: 230,
+})
+
+colombia.push({
+    name:"Luna",
+    salary: 240,
+})
+
+colombia.push({
+    name:"Lariza",
+    salary: 290,
+})
+
+colombia.push({
+    name:"Lala",
+    salary: 280,
+})
+
+colombia.push({
+    name:"Laura",
+    salary: 260,
+});
+
+colombia.push({
+    name:"Leandro",
+    salary: 300,
+});
+
+colombia.push({
+    name:"Luis",
+    salary: 250,
+});
